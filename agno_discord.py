a354e4ed4834b4bdd2f92cc5dc1d88a66c67c02b
EricK <66191863+ErickGuimaraesFerreira@users.noreply.github.com>
2025-12-31 15:25:58 -0300

    Update agno_discord.py

--- agno_discord.py
+++ agno_discord.py
@@ -16,17 +16,17 @@ load_dotenv()
 db = SqliteDb(session_table="agent_sessions", db_file="tmp/agent.db")  
 
 
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")  # Obtém o token do bot do Discord das variáveis de ambiente
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  # Obtém a chave da API do Google das variáveis de ambiente
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")  
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
 
 
 agent = Agent( 
-    name="Discord Agent",  # Define o nome do agente
+    name="Discord Agent",  
     description="Bot Discord que responde a mensagens em canais de texto.",  
-    instructions="Você é um assistente de IA que responde a mensagens em canais de texto. Seu objetivo é ajudar os usuários a resolverem suas dúvidas e resolverem problemas.",  # Define as instruções de comportamento do agente
+    instructions="Você é um assistente de IA que responde a mensagens em canais de texto. Seu objetivo é ajudar os usuários a resolverem suas dúvidas e resolverem problemas.",  
     tools=[DiscordTools(DISCORD_BOT_TOKEN), DuckDuckGoTools()],  
     model=Gemini(id="gemini-2.5-flash", api_key=GOOGLE_API_KEY),  
-    db=db,  # Define o banco de dados para persistência
+    db=db,  
     num_history_runs=4,  
     num_history_messages=10,  
     enable_user_memories=True, 
@@ -35,10 +35,11 @@ agent = Agent(
 
 )
 
-discord_client = DiscordClient(agent)  # Inicializa o cliente Discord com o agente configurado
+discord_client = DiscordClient(agent)  # inicia o cliente Discord com o agente configurado
 
-if __name__ == "__main__":  # Verifica se o script está sendo executado diretamente
+if __name__ == "__main__":  
     discord_client.serve()  
 #agent.print_response(f"Send Hello World to channel {channel_id}")
+
 
 
